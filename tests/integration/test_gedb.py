e69a9f4193f9e29ac10c1c2da6ca052f41cb2f95
Matthew Gerring <matthew.gerring@jax.org>
2024-03-15 12:19:07 -0400

    Added another example of running gedb client.

--- tests/integration/test_gedb.py
+++ tests/integration/test_gedb.py
@@ -1,10 +1,14 @@
 from unittest.mock import patch
 
 import pytest
+import os
+
 from typer.testing import CliRunner
 from geneweaver.client.gedb import *
 from typing import List
 from typing import Set
+from pathlib import Path
+from collections import OrderedDict
 
 runner = CliRunner()
 
@@ -23,5 +27,40 @@ def test_get_tissues():
     tissues:  [str] = client.distinct("tissue")
     assert "heart" in tissues, "Tissue set must contain heart"
     assert "striatum" in tissues, "Tissue set must contain striatum"
-
     
+def test_search_expressions():
+    
+    gedbc = GeneExpressionDatabaseClient("https://geneweaver-dev.jax.org/gedb")
+    genes = get_genes("tests/integration/connective_tissue_disorder_log2fc_mouse.csv")
+    all_strains = gedbc.distinct("strain")
+    
+    drequest = DataRequest(geneIds     = genes, 
+                           strains     = all_strains, 
+                           sourceType  = SourceType.IMPUTED)
+    
+    imputations: List[DataResult] = gedbc.search(drequest)    
+
+    print(imputations)
+    # TODO Test result
+
+
+def get_genes(path: str) -> OrderedDict:
+    
+    # We keep the keys in order here as
+    # it is easier to use the debugger and
+    # check the dict
+    gene_values = OrderedDict()
+    with open(path,'r') as file:
+ 
+        # reading each line from original text file
+        for line in file.readlines():
+            
+            line = line.strip()
+            if not (line.startswith('#')):
+                sa = line.split(',')
+                #     {"gene_id": sa[0],
+                #      "log2fc" : sa[1],
+                #      "species": sa[2]}
+                gene_values[sa[0]] = sa[1]
+                
+    return gene_values
